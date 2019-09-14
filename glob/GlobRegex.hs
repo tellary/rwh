bbb39e98baf54d62cd36334c34db224148099fb3
Ilya Silvestrov <tellary@gmail.com>
2019-09-14 09:06:33 -0700

    `matchesGlob "f!!.c" "f[!][!].c"`

--- glob/GlobRegex.hs
+++ glob/GlobRegex.hs
@@ -1,26 +1,36 @@
-module GlobRegex(globToRegex) where
+module GlobRegex(globToRegex, matchesGlob) where
+
+import Text.Regex.Posix
 
 globToRegex g = '^':globToRegex' g ++ "$"
 
 globToRegex' "" = ""
-globToRegex' (c:str)
-  | c == '[' = '[':globClassOpen str
-  | c == ']' = error "Unexpected class closing"
-  | c == '*' = ".*" ++ globToRegex' str
-  | c == '?' = ".?" ++ globToRegex' str
-  | otherwise = (escape c) ++ globToRegex' str
+globToRegex' ('[':str) = '[':globClassOpen str
+globToRegex' ('*':str) = ".*" ++ globToRegex' str
+globToRegex' ('?':str) = ".?" ++ globToRegex' str
+globToRegex' (c:str) = (escape c) ++ globToRegex' str
 
 escape c
   | escapeChar c = '\\':[c]
   | otherwise = [c]
-  where escapeChar = (`elem` "\\\n\r.*?!")
+  where escapeChar = (`elem` "\\.+()^${}]|")
 
-globClassOpen "" = error "Unterminated glob class"
+globClassOpen "" = error "Unterminated character class"
 globClassOpen (']':_) = error "Empty class"
-globClassOpen ('!':str) = '^':globClassOpenN str
+globClassOpen ('!':str) = globClassOpenN ('!':str)
 globClassOpen str = globClass str
-globClassOpenN ('!':str) = globClass ('!':str)
-globClassOpenN str = globClassOpen str
+-- matchesGlob "f!!.c" "f[!][!].c"
+-- matchesGlob "fa.c" "f[!!].c"
+globClassOpenN ('!':']':str) = '!':']':globToRegex' str
+globClassOpenN "!" = globClassOpen ""
+globClassOpenN ('!':str) = '^':globClass str
 globClass "" = globClassOpen ""
 globClass (']':str) = ']':globToRegex' str
 globClass (c:str) = (escape c) ++ globClass str
+
+matchesGlob :: FilePath -> String -> Bool
+matchesGlob name pat = name =~ globToRegex pat
+
+bad = globToRegex "fo[.c"
+
+-- matchesGlob "fo].c" "fo].c"
