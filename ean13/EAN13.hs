ce54c2f870cec10eda6b40c8954aadc3b1ec36bb
Ilya Silvestrov <tellary@gmail.com>
2019-11-10 21:30:05 -0800

    `encodeEAN13`

--- ean13/EAN13.hs
+++ ean13/EAN13.hs
@@ -1,8 +1,50 @@
 module EAN13 where
 
+import Data.Array ((!), elems, listArray)
+
 -- https://en.wikipedia.org/wiki/International_Article_Number#Calculation_of_checksum_digit
 checkDigit :: Integral a => [a] -> a
 checkDigit digits = tenToZero $ 10 - sum products `mod` 10
   where products = zipWith ($) (cycle [(*3), (*1)]) $ reverse digits
         tenToZero 10 = 0
         tenToZero d  = d
+
+array xs = listArray (0, l - 1) xs
+  where l = length xs
+
+leftOddCodes = array [
+  "0001101", "0011001", "0010011", "0111101", "0100011",
+  "0110001", "0101111", "0111011", "0110111", "0001011"]
+
+rightCodes = fmap compliment <$> leftOddCodes
+  where compliment '0' = '1'
+        compliment '1' = '0'
+
+parity = array [
+  "111111", "110100", "110010", "110001", "101100",
+  "100110", "100011", "101010", "101001", "100101"]
+
+leftEvenCodes = array $ reverse <$> elems rightCodes
+
+sideMarker   = "101"
+centerMarker = "01010"
+
+encodeEAN13 :: [Int] -> Maybe String
+encodeEAN13 is
+  | length is /= 12 = Nothing
+  | any ((< 0)) is  = Nothing
+  | any ((> 9)) is  = Nothing
+  | otherwise       = Just $ concat $ encodeDigits is
+
+encodeDigits :: [Int] -> [String]
+encodeDigits c@(first:is) =
+  sideMarker:leftEncoded ++ centerMarker:rightEncoded ++ [sideMarker]
+  where
+    (left, right) = splitAt 6 is
+    leftEncoded   = zipWith encodeLeft (parity ! first) left
+    rightEncoded  = encodeRight <$> right ++ [checkDigit c]
+
+encodeLeft '0' = (leftEvenCodes !)
+encodeLeft '1' = (leftOddCodes  !)
+
+encodeRight d = rightCodes ! d
--- ean13/EAN13QC.hs
+++ ean13/EAN13QC.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 import EAN13
+import Control.Exception (assert)
+import Data.Array ((!))
 import Test.QuickCheck
 
 rwhIsbn = [9,7,8,0,5,9,6,5,1,4,9,8,3]
@@ -12,10 +14,39 @@ purelyFunctionalStructures2 = [9,7,8,0,5,2,1,6,6,3,5,0,2]
 
 newtype EAN13 a = EAN13 [a] deriving Show
 
-instance Arbitrary (EAN13 Integer) where
+instance Arbitrary (EAN13 Int) where
   arbitrary = elements [
-    EAN13 rwhIsbn, EAN13 ean13_1,
+    EAN13 rwhIsbn, EAN13 ean13_1, EAN13 ean13_2,
     EAN13 purelyFunctionalStructures1,
     EAN13 purelyFunctionalStructures2]
 
+prop_checkDigit :: EAN13 Int -> Bool
 prop_checkDigit (EAN13 is) = (checkDigit $ take 12 is) == last is
+
+-- https://en.wikipedia.org/wiki/International_Article_Number#EAN-13_barcode_example
+ean13_2 :: [Int]
+ean13_2 = [4,0,0,3,9,9,4,1,5,5,4,8,6]
+ean13_2_parts = [
+  sideMarker       ,
+  leftOddCodes  ! 0,
+  leftEvenCodes ! 0,
+  leftOddCodes  ! 3,
+  leftOddCodes  ! 9,
+  leftEvenCodes ! 9,
+  leftEvenCodes ! 4,
+  centerMarker     ,
+  rightCodes    ! 1,
+  rightCodes    ! 5,
+  rightCodes    ! 5,
+  rightCodes    ! 4,
+  rightCodes    ! 8,
+  rightCodes    ! 6,
+  sideMarker]
+
+t1 = assert
+     ((encodeDigits $ take 12 ean13_2) == ean13_2_parts)
+     "encodeDigits succeed"
+
+tests = do
+  quickCheck prop_checkDigit
+  return t1
