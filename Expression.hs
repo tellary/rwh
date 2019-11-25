f3a04c0f1742867e68930ece2b621a8baa2a60db
Ilya Silvestrov <tellary@gmail.com>
2019-11-24 21:44:11 -0800

    `ReadPrec` expercise -- `Expression.hs`

---
+++ Expression.hs
@@ -0,0 +1,35 @@
+import Text.Read.Lex (Lexeme(Symbol))
+import Text.Read
+
+newtype Expression = Expression Int deriving Show
+
+result (Expression n) = n
+
+readNum = do
+  n <- readPrec
+  return $ Expression n
+
+readSumR = do
+  n1 <- readPrec
+  Symbol "+" <- lexP
+  Expression n2 <- step readSum
+  return $ Expression $ n1 + n2
+
+readSumSumL = do
+  Punc "(" <- lexP
+  Expression sumL <- readSum
+  Punc ")" <- lexP
+  Symbol "+" <- lexP
+  Expression sumR <- readSum
+  return $ Expression $ sumL + sumR
+
+-- sum -> ([`(`] num + sum | num [`)`]) | `( sum )` + sum
+readSum = (parens $ readSumR +++ readNum) +++ readSumSumL
+
+instance Read Expression where
+  readPrec = readSum
+
+
+-- result $ read "(1 + 2) + 4"
+-- result $ read "1 + (2 + 4)"
+-- result $ read "(1 + 2 + 4)"
