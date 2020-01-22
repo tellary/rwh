2d1a0d143851ed28683821cd755fbf9fa6ceedb0
Ilya Silvestrov <tellary@gmail.com>
2020-01-21 17:00:44 -0800

    Add `URLQueryParser`

---
+++ parsec/URLQueryParser.hs
@@ -0,0 +1,32 @@
+module URLQueryParser where
+
+import Text.ParserCombinators.Parsec
+import Numeric (readHex)
+
+query :: CharParser st [(String, Maybe String)]
+query = sepBy fieldValue (char '&') <* eof
+fieldValue :: CharParser st (String, Maybe String)
+fieldValue = (,) <$> field <*> optionMaybe (char '=' *> value)
+
+urlChar =
+      directChar
+  <|> spaceChar
+  <|> hexChar
+  <|> (noneOf "=&" >>= \c -> fail ("Invalid URL char: " ++ [c]))
+
+isDirectChar c =
+     c `elem` ['A'..'Z']
+  || c `elem` ['a'..'z']
+  || c `elem` ['0'..'9']
+  || c `elem` "*-._"
+
+directChar = satisfy isDirectChar
+spaceChar  = (char '+') *> pure ' '
+hexChar    = char '%' *>
+             (toEnum . fst . head . readHex <$> count 2 hexDigit)
+
+field = many urlChar
+value = many urlChar
+
+-- λ> parse query "" "field1=value1+2+%21&field2=value2%22&field3"
+-- Right [("field1",Just "value1 2 !"),("field2",Just "value2\""),("field3",Nothing)]
