658b4e1d228e1dc79aa8a33cf965733f83e1877a
Ilya Silvestrov <tellary@gmail.com>
2020-05-29 22:23:04 -0700

    Compile `findean13` to JavaScript

---
+++ ch12/default.nix
@@ -0,0 +1,14 @@
+with (import (builtins.fetchTarball {
+  url = "https://github.com/dmjio/miso/archive/561ffad.tar.gz";
+  sha256 = "1wwzckz2qxb873wdkwqmx9gmh0wshcdxi7gjwkba0q51jnkfdi41";
+}) {});
+let
+  dontCheck = pkgs.haskell.lib.dontCheck;
+  pkgs1 = pkgs.haskell.packages.ghcjs.override {
+    overrides = self: super: {
+      mynetpbm = super.callCabal2nix "mynetpbm" ../ch10 {};
+      # Disabling tests for build speed
+      QuickCheck = dontCheck super.QuickCheck;
+    };
+  };
+in pkgs1.callCabal2nix "ean13" ./. {}
