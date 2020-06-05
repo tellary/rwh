4c8222f342c7aa81e5537c6a81ca4ea53561a32c
Ilya Silvestrov <tellary@gmail.com>
2020-06-04 19:43:56 -0700

    Show progress
    Show progress of loading an image and recognizing a barcode

--- ean13-ui/Main.hs
+++ ean13-ui/Main.hs
@@ -3,46 +3,52 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import Control.Concurrent.MVar (newEmptyMVar, putMVar, readMVar)
+import Control.Concurrent.MVar (MVar, newEmptyMVar, putMVar, readMVar)
 import Control.Exception       (SomeException, catch, displayException)
-import Helper                  (ean13, parseImage)
-import Miso                    (App (..), View, accept_, asyncCallback, br_,
-                                consoleLog, defaultEvents, div_, getElementById,
-                                id_, img_, input_, noEff, onChange, src_,
-                                startApp, text, type_, (<#))
-import Miso.String             (MisoString, fromMisoString, ms)
 import GHCJS.Foreign.Callback  (Callback)
 import GHCJS.Types             (JSVal)
+import Helper                  (ean13, parseImage)
+import Miso                    (App (..), View, accept_, asyncCallback,
+                                batchEff, br_, consoleLog, defaultEvents, div_,
+                                getElementById, id_, img_, input_, noEff,
+                                onChange, src_, startApp, text, type_, (<#))
+import Miso.String             (MisoString, fromMisoString, ms)
 import Text.Printf             (printf)
 
-newtype ImageString = ImageString { imageStr :: MisoString } deriving (Eq, Show)
+newtype ImageString
+  = ImageString { imageStr :: MisoString } deriving (Eq, Show)
 
 data Model
-  = ErrorModel           MisoString
-  | NoBarcodeModel       NoBarcode
-  | ResultModel          Result
-  | HighErrorResultModel Result
-  deriving (Eq, Show)
-
-data NoBarcode
-  = NoBarcode
-  { noBarcodeImage :: ImageString
-  , noBarcodeError :: MisoString
+  = NoFileChoosen
+  | Model
+  { image  :: Loading ImageString
+  , result :: Loading Result
   } deriving (Eq, Show)
 
-data Result
-  = Result
-  { resultImage :: ImageString
-  , resultEan13 :: [Int]
-  , resultError :: Double
+data Loading a
+  = Loading
+  | Loaded a
+  deriving (Eq, Show)
+
+data Result = Error MisoString | Bad EAN13 | Good EAN13 deriving (Eq, Show)
+
+data EAN13
+  = EAN13
+  { eanDigits :: [Int]
+  , eanError  :: Double
   } deriving (Eq, Show)
 
 data Action
   = ReadFile
   | NoOp
-  | SetContent Model
+  -- `ShowProgress` has two `MVar` to carry them out `ReadFile`
+  -- action handler and "batch" them into separate events
+  -- `ShowProgress` handler.
+  | ShowProgress (MVar ImageString) (MVar Result)
+  | SetImage  ImageString
+  | SetResult Result
 
-app = App { model         = ErrorModel "No barcode image loaded"
+app = App { model         = NoFileChoosen
           , initialAction = NoOp
           , update        = updateModel
           , view          = viewModel
@@ -56,12 +62,14 @@ main = startApp app
 updateModel ReadFile m = m <# do
   consoleLog "ReadFile"
   fileReaderInput <- getElementById "fileReader"
-  file <- getFile fileReaderInput
-  reader <- newReader
-  mvar <- newEmptyMVar
+  file            <- getFile fileReaderInput
+  reader          <- newReader
+  imageMVar       <- newEmptyMVar
+  resultMVar      <- newEmptyMVar
   setOnLoad reader =<< do
     asyncCallback $ do
       r <- ImageString <$> getResult reader
+      putMVar imageMVar r
       catch
         (do
           case parseImage . fromMisoString . imageStr $ r of
@@ -70,23 +78,28 @@ updateModel ReadFile m = m <# do
                 Right (err :: Double, ean13 :: [Int]) -> do
                   consoleLog . ms @String
                     $ printf "Found EAN13: %s\nError: %f" (show ean13) err
-                  if err < 0.15 -- 14.25 extra lines in a barcode of 95 lines
-                    then putMVar mvar . ResultModel $ Result r ean13 err
-                    else putMVar mvar
-                         . HighErrorResultModel $ Result r ean13 err
+                  -- 14.25 extra lines in a barcode of 95 lines
+                  if err < 0.15
+                    then putMVar resultMVar . Good $ EAN13 ean13 err
+                    else putMVar resultMVar . Bad  $ EAN13 ean13 err
                 Left err ->
-                  putMVar mvar . NoBarcodeModel $ NoBarcode r (ms $ err)
-            Left err ->
-              putMVar mvar . ErrorModel . ms $ err)
+                  putMVar resultMVar . Error . ms $ err
+            Left err -> putMVar resultMVar . Error . ms $ err)
         (\(e :: SomeException) -> do
             let msg = "Failed to load image or process EAN13: "
                       ++ displayException e
             consoleLog . ms $ msg
-            putMVar mvar . ErrorModel . ms $ msg
+            putMVar resultMVar . Error . ms $ msg
         )
   readDataURL reader file
-  SetContent <$> readMVar mvar
-updateModel (SetContent m) _ = noEff m
+  return $ ShowProgress imageMVar resultMVar
+updateModel (ShowProgress imageMVar resultMVar) _
+  = batchEff (Model Loading Loading)
+    [ SetImage  <$> readMVar imageMVar
+    , SetResult <$> readMVar resultMVar
+    ]
+updateModel (SetImage  img) m = noEff m { image  = Loaded img }
+updateModel (SetResult r  ) m = noEff m { result = Loaded r   }
 updateModel NoOp m = noEff m
 
 viewModel :: Model -> View Action
@@ -102,21 +115,30 @@ viewModel m
     , br_ []
     ] ++ imgView m
   where
-    imgView (ErrorModel err) = [text err]
-    imgView (NoBarcodeModel (NoBarcode img err)) =
-      [ text err, br_ []
+    imgView NoFileChoosen =
+      [ text "No barcode image choosen" ]
+    imgView (Model Loading      Loading) =
+      [ text "Loading image ..." ]
+    imgView (Model (Loaded img) Loading) =
+      [ text "Recognizing EAN13 barcode ...", br_ []
       , img_ [src_ . imageStr $ img]
       ]
-    imgView (ResultModel (Result img ean13 err)) =
+    imgView (Model Loading      (Loaded result)) =
+      eanView result ++
+      [ text "Image is still loading (weird) ..." ]
+    imgView (Model (Loaded img) (Loaded result)) =
+      eanView result ++
+      [ img_ [src_ . imageStr $ img] ]
+    eanView (Error err) =
+      [ text err, br_ [] ]
+    eanView (Good (EAN13 ean13 err)) =
       [ text . ms $ "Barcode: " ++ show ean13, br_ []
       , text . ms $ "Error: "   ++ show err, br_ []
-      , img_ [src_ . imageStr $ img]
       ]
-    imgView (HighErrorResultModel (Result img ean13 err)) =
+    eanView (Bad  (EAN13 ean13 err)) =
       [ text        "ERROR TOO LARGE, barcode may be innacurate", br_ []
       , text . ms $ "Barcode: " ++ show ean13, br_ []
       , text . ms $ "Error: "   ++ show err, br_ []
-      , img_ [src_ . imageStr $ img]
       ]
 
 foreign import javascript unsafe "$r = new FileReader();"
