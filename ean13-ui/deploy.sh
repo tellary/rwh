a10d39cfa66f4d4a341f6ad6da9dff3b477a6765
Ilya Silvestrov <tellary@gmail.com>
2020-06-30 19:30:37 -0700

    Add cache control on deploy

--- ean13-ui/deploy.sh
+++ ean13-ui/deploy.sh
@@ -1,8 +1,9 @@
 set -x
 
 nix-build
-aws s3 mb s3://ean13
+BUCKET=ean13
+aws s3 mb s3://$BUCKET
 pushd result/bin/findean13-ui.jsexe/
-ls * | xargs -I{} -n 1 -P 10 aws s3 cp {} s3://ean13/{}
-ls * | xargs -I{} -n 1 -P 10 aws s3api put-object-acl --bucket ean13 --key {} --acl public-read
+ls * | xargs -I{} -n 1 -P 10 aws s3 cp {} s3://$BUCKET/{} --metadata-directive REPLACE --cache-control max-age=60
+ls * | xargs -I{} -n 1 -P 10 aws s3api put-object-acl --bucket $BUCKET --key {} --acl public-read
 popd
