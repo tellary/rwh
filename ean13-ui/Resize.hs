ff161463ad83ec0fa4a4eb4d7908653c7011ec8f
Ilya Silvestrov <tellary@gmail.com>
2020-07-02 22:26:38 -0700

    Make `Resize.hs` compile with GHC

--- ean13-ui/Resize.hs
+++ ean13-ui/Resize.hs
@@ -10,26 +10,27 @@ import GHCJS.DOM.FileReader    (getResult, load, newFileReader, readAsDataURL)
 import GHCJS.DOM.Types         (ArrayBuffer (..), BlobPropertyBag)
 import GHCJS.Marshal.Pure      (pToJSVal)
 import GHCJS.Types             (JSString, JSVal)
+import Miso                    (JSM, ghcjsPure)
 import Unsafe.Coerce           (unsafeCoerce)
 
-byteStringJSBuffer :: ByteString -> IO JSVal
+byteStringJSBuffer :: ByteString -> JSM JSVal
 byteStringJSBuffer bs = do
-  arr <- getArrayBuffer <$> thaw buffer
+  (buffer, _, _) <- ghcjsPure $ fromByteString bs
+  arr <- ghcjsPure =<< (getArrayBuffer <$> thaw buffer)
   return . pToJSVal $ arr
-  where (buffer, _, _) = fromByteString bs
 
 byteStringBlob bs = do
   buf <- byteStringJSBuffer bs
   newBlob [ArrayBuffer buf] (Nothing :: Maybe BlobPropertyBag)
 
-byteStringJSDataUrl :: ByteString -> IO JSString
+byteStringJSDataUrl :: ByteString -> JSM JSString
 byteStringJSDataUrl bs = do
   r <- newFileReader
   b <- byteStringBlob bs
   readAsDataURL r (Just b)
-  v <- newEmptyMVar
+  v <- liftIO $ newEmptyMVar
   on r load $ do
     result <- getResult r
     liftIO $ putMVar v result
-  Just result <- readMVar v
+  Just result <- liftIO . readMVar $ v
   return . unsafeCoerce . pToJSVal $ result
