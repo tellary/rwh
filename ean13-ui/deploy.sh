32a08f1a5d5c077addc67c219381d5407a312514
Ilya Silvestrov <tellary@gmail.com>
2020-07-13 13:53:19 -0700

    Enable S3 website hosting

--- ean13-ui/deploy.sh
+++ ean13-ui/deploy.sh
@@ -3,6 +3,7 @@ set -x
 nix-build -A release
 BUCKET=ean13
 aws s3 mb s3://$BUCKET
+aws s3api put-bucket-website --bucket $BUCKET --website-configuration file://website.json
 pushd result/bin/findean13-ui.jsexe/
 ls * | xargs -I{} -n 1 -P 10 aws s3 cp {} s3://$BUCKET/{} --metadata-directive REPLACE --cache-control max-age=60
 ls * | xargs -I{} -n 1 -P 10 aws s3api put-object-acl --bucket $BUCKET --key {} --acl public-read
